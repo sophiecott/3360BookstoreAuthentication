9d138345aa26af7c41c8359669b7d21948d57cbd
sophiecott <97978942+sophiecott@users.noreply.github.com>
2025-03-19 17:58:12 -0600

    Add project files.

---
+++ Bookstore/Models/DataLayer/BookstoreContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Models
+{
+    public class BookstoreContext : IdentityDbContext<User>
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Genre> Genres { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<Author> Authors { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // configure entities
+            modelBuilder.ApplyConfiguration(new ConfigureGenres());
+            modelBuilder.ApplyConfiguration(new ConfigureBooks());
+            modelBuilder.ApplyConfiguration(new ConfigureAuthors());
+            modelBuilder.ApplyConfiguration(new ConfigureBookAuthors());
+        }
+    }
+}
---
+++ Bookstore/Models/DataLayer/Configuration/ConfigureIdentity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bookstore.Models
+{
+    public class ConfigureIdentity
+    {
+        public static async Task CreateAdminUserAsync(IServiceProvider provider) // new async task to create the adminuserasync method
+        {
+            var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>(); // create new variable with the rolemanager provide
+            var userManager = provider.GetRequiredService<UserManager<User>>(); // create new variable for the UserManager provider
+
+            string username = "admin"; // for the admin make the username "admin"
+            string password = "Sesame"; // for the admin login password make is "Sesame"
+            string roleName = "Admin"; // the role name is Admin
+
+            if (await roleManager.FindByNameAsync(roleName) == null) // if the rolemanager is equal to null
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName)); // await new identity role
+            }
+
+            if (await userManager.FindByNameAsync(username) == null) // if the usermanager has a username it's equal to null
+            {
+                User user = new User { UserName = username }; // create new user
+                var result = await userManager.CreateAsync(user, password); // with the username and password
+
+                if (result.Succeeded) // if the result succeeded
+                {
+                    await userManager.AddToRoleAsync(user, roleName); // add the role name user to the Roles
+                }
+            }
+        }
+    }
+
+}
